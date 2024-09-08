9269d01b98c2c8d8600751370134400ac7d2229a
Ayush Pandey <ayush@Ayushs-MacBook-Air.local>
2024-09-08 18:29:14 +0530

    test 14

--- index.js
+++ index.js
@@ -104,11 +104,9 @@ async function getAppleMusicTrackInfo(url) {
   console.log(`url is ${url}`)
   const id = url.slice(-10);
   try {
-    const response = await fetch(`/api/apple-music?id=${id}`);
+    const trackInfo = await response.text();
+    return trackInfo || 'Track name not found';
 
-    let trackInfo = response;
-    console.log(trackInfo);
-    return trackInfo;
   } catch (error) {
     console.error('Error fetching Apple Music data:', error);
     throw error;  // Rethrow the error to handle it where the function is called
